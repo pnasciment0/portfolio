da6b348ea7d12c47c78aa34108d2eaeafcacfa51
Paulo Nascimento <pnascimento@uchicago.edu>
2024-05-11 21:53:48 -0700

    resume page looking good

--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -3,6 +3,7 @@ import { Worker, Viewer } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import { IoMdDownload } from "react-icons/io";
 
 const ResumePage: React.FC = () => {
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
@@ -10,6 +11,7 @@ const ResumePage: React.FC = () => {
   return (
     <div className="container mx-auto px-4">
       <h1 className="text-lg resume-text font-bold my-4">Current Resume</h1>
+      <p className="resume-description">To download my resume, hit "Download" at the top of the PDF viewer or click the button below it.</p>
       <div className="resume-viewer mb-4">
         <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.min.js">
           <Viewer
@@ -18,6 +20,13 @@ const ResumePage: React.FC = () => {
           />
         </Worker>
       </div>
+      <a
+        href={`${process.env.PUBLIC_URL}/resume.pdf`}
+        download={"PauloNascimentoResume.pdf"}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded resume-dl"
+      >
+        Download Resume <IoMdDownload/>
+      </a>
     </div>
   );
 }
